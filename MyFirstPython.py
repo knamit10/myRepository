9e56f86ea16d4dc958a7d3ee99d434807cc046ae
knamit10 <36757104+knamit10@users.noreply.github.com>
2018-02-23 14:14:11 +0530

    Added My First Python

---
+++ MyFirstPython.py
@@ -0,0 +1,16 @@
+def print_line(line):
+    print line
+
+
+if __name__ == '__main__':
+        print_line('welcome world!...')
+
+
+def print_line(line):
+    print line
+
+
+if __name__ == '__main__':
+    print_line('welcome world!...')
+
+
