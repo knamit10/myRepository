1b519ed54ac4166f2e8fe454d7e5ea8f391806cd
knamit10 <knamit10@github.com>
2018-02-23 15:34:18 +0530

    hii...
    Added Extra code to display "Hello World 1" and consecutive

--- MyFirstPython.py
+++ MyFirstPython.py
@@ -3,7 +3,7 @@ def print_line(line):
 
 
 if __name__ == '__main__':
-        print_line('welcome world!...')
+        print_line('welcome world! 1...')
 
 
 def print_line(line):
@@ -11,6 +11,14 @@ def print_line(line):
 
 
 if __name__ == '__main__':
-    print_line('welcome world!...')
+    print_line('welcome world! 2...')
+
+
+def print_line(line):
+    print line
+
+
+if __name__ == '__main__':
+    print_line('welcome world! 3...')
 
 
